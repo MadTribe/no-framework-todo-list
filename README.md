9a4ac49d15f2dafabb8b4c5e5ab0c85c9038a7e8
MadTribe <MadTribe@users.noreply.github.com>
2020-12-24 08:48:56 +0800

    Added initial code base.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # no-framework-todo-list
+
+# Introduction
+
+It is very common for developers to ask "What Framework Should I use?"
+On the front end in 2021 this might mean React, Angular, Vue or Svelte. In the past it was jQuery.
+
+These frameworks offer a lot out of the box, but they also add unused download bloat, enforce certain structures (can be useful but discourages developer creativity) and puts a layer of abstraction between the developer and the web platform.
+
+For developers one of the great attractions of frameworks is how much code they can save. 
+
+So in this repo I want to explore how to efficiently create a todo list application with just plain old javascript. I've chosen a todo list application because it is a common example for many frameworks to show.
+
+Currently the app has only **127 lines of js** code and is **< 5kb** download <u>uncompressed</u> (apart from a random winter decoration I added for my son.)
+
+Compare this to the minified Vue.js framework (one of the better ones) which  is ~40kb out of the box before you write a single line of code. 
+
+The no-framework approach does have drawbacks, but it means ultimately you will invest your time in learning how the web platform actually works and how to get more out of javascript by, creating useful abstractions that keep your work efficient. 
+
+Ultimately you may decide that a framework is right for your project, but knowing how and when to work without one is a great asset to any developer. 
+
+# Rules
+
+No external JS frameworks or libraries. 
+
+Note I started out by including the [tactic CSS library]([GitHub - yegor256/tacit: CSS Framework for Dummies, Without Classes](https://github.com/yegor256/tacit) which is a non opinionated library for just making raw html look ok. I may remove it later. 
+
+
+
+## Current Screenshot
+
+![screenshot_v1.png](./docs/screenshot_v1.png)
---
+++ script/main.js
@@ -0,0 +1,163 @@
+(function(doc,win){
+
+    function find(query){
+        return doc.querySelector(query);
+    };
+
+    function e(tagName, text, attrs){
+        if (arguments.length < 2){
+            text = null;
+        }
+        if (arguments.length < 3){
+            attrs = {};
+        }  
+        return function(){
+            var children = arguments;
+            if (arguments.length > 0){
+                if (Array.isArray (arguments[0])){
+                    children = arguments[0]
+                }
+            }
+            
+            var elem = doc.createElement(tagName);
+
+            for (const attrName in attrs) {
+                elem.setAttribute(attrName,attrs[attrName])
+            }
+
+            if (typeof children != 'undefined'){
+                for(let i = 0; i < children.length; i++){ 
+                    var child = children[i]
+                    if (typeof child === 'function'){
+                        elem.appendChild(child())
+                    } else {
+                        elem.appendChild(child)
+                    }
+                };
+            }
+            if (text != null && text !== ""){
+                elem.innerText = text;
+            }
+            return elem;
+        }
+    };
+
+    function todo_item(item, bus){
+    
+        var setState = function(item, state){
+            var cls = "done";
+            if (state){
+                cls = "done";
+            } else {
+                cls = "not-done";
+            }  
+            item.className = cls         
+        }
+
+        var draw = function(){
+                var check=e('span','')()
+                check.className="check";  
+                var clickable = e('div')(
+                    check,
+                    e('label',item['name'])        
+                );     
+                var delBtn = e('button','delete')();
+                
+                delBtn.onclick = function(){
+                    bus.dispatchEvent(new CustomEvent('delete', {'detail':item}));
+                }
+                
+                var elem = e('div')(
+                        clickable,
+                        delBtn
+                    )
+                
+                clickable.onclick = function(){
+                    item['done'] = !item['done'];
+                    setState(elem,item['done']);
+
+                    bus.dispatchEvent(new Event('redraw'));
+                }
+                
+                setState(elem,item['done']);
+                return elem;
+        }
+
+        return draw;
+    }
+
+    /**
+     * 
+     * @param {Array} model 
+     */
+    function todo_list(model, bus){
+        var elems = model.map(function(e) {
+            return todo_item(e, bus);
+        });
+        return e('ol')(
+            elems
+        )
+    }
+
+    function addTaskBtn(bus, model){
+        var input = e('input',null,{'type':'textbox'})();
+        input.focus();
+        var btn = e('button',"Add Task")();
+        
+        btn.onclick=function(){
+            console.log('new task ', input.value);
+            if (input.value != ""){
+                model.push({"name": input.value,"done":false})
+
+                bus.dispatchEvent(new Event('redraw'));
+            }
+        }
+        return e('div')(
+            input,
+            btn
+        ) 
+    }
+
+    var app = find('#app');
+
+    var model = [
+        {"name":"washing up","done":true},
+        {"name":"sweep floor","done":false},
+        {"name":"homework","done":true},
+        {"name":"party","done":false}
+    ]
+
+    var container = null;
+
+    function redraw(app){
+        if (container != null){
+            app.removeChild(container)
+        }
+
+        container = e('div')(
+            e('h2','No Framework Todo List'),
+            addTaskBtn(app, model),
+            todo_list(model, app)   
+        )
+
+        app.appendChild(container)
+    }
+
+    redraw(app)
+
+    app.addEventListener('redraw', function (e) {
+        console.log('redraw');
+        redraw(app);
+    }, false);
+
+
+    app.addEventListener('delete', function (e) {
+        console.log('delete event', e.detail);
+        model = model.filter(task => task != e.detail);
+        redraw(app);
+
+    }, false);
+
+
+
+})(document, window)
